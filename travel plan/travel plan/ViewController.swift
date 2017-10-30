a5087394d8249f1895ba6e6086d3c0dc7903382e
MadNoodle <mjanneau@gmail.com>
2017-10-30 12:34:35 +0100

    Mise en place de la View et du View controller

---
+++ travel plan/travel plan/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  travel plan
+//
+//  Created by Mathieu Janneau on 25/10/2017.
+//  Copyright © 2017 Mathieu Janneau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  //UI element linking
+  @IBOutlet weak var datePicker: UIDatePicker!
+  @IBOutlet weak var slider: UISlider!
+  @IBOutlet weak var price: UILabel!
+  
+  //slider interaction
+  @IBAction func sliderValue(_ sender: Any) {
+    // display the value in label
+    self.price.text = String(Int(self.slider.value)) + " €"
+  }
+  
+  //Button on click event
+  @IBAction func search(_ sender: UIButton) {
+    var dateChoice : Date
+    var maxPrice : Int
+    
+    //getting date info from picker
+    dateChoice = datePicker.date
+    
+    //getting price info from slider
+    maxPrice = Int(self.slider.value)
+    
+    print(dateChoice)
+    print(maxPrice)
+    //function to look for proposal
+  }
+  
+}
+
+
